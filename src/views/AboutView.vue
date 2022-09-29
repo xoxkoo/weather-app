864eabf94a99d1a702305cf4d3e42067087a73ac
xoxkoo <anton.durcak22@gmail.com>
2022-09-29 14:08:54 +0200

    about view almost done, missing forecast

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <AppHeader></AppHeader>
+  <div class="container about">
+    <p>Api from: <a class="link" href="https://openweathermap.org">OpenWeatherMap</a></p>
+    <p><img src="../assets/img/location-2969395.png" alt="location" class="icon"> icon from: <a href="https://iconscout.com/3d/location-3543264" class="link">iconscout</a></p>
+    <p><img src="../assets/img/location-pin.png" alt="location" class="icon"> icon from: <a href="https://iconscout.com/3d/location-3543018" class="link">iconscout</a></p>
+  </div>
   <footer>
     <p>designed by me. &copy; <a href="adur.glitch.me" class="link">xoxo</a></p>
   </footer>
@@ -13,6 +18,22 @@ export default {
 </script>
 
 <style lang="sass">
+@import '@/assets/sass/variables'
+.container.about
+  text-align: left
+
+  & .icon
+    margin-right: .5em
+    width: 27px
+
+  & p
+    font-size: 1.1rem
+    color: $c_dark
+    display: flex
+    align-items: center
+
+  & .link
+    padding-left: .5em
 
 footer
   font-size: 1.1rem
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       prependData: `
-  //         @import '@/assets/s'
-  //       `
-  //     }
-  //   }
-  // }
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
+//   transpileDependencies: true
+//   // css: {
+//   //   loaderOptions: {
+//   //     sass: {
+//   //       prependData: `
+//   //         @import '@/assets/s'
+//   //       `
+//   //     }
+//   //   }
+//   // }
+// })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
+};
