0dc99901609138b321c4f7b117e236995fb8758f
xoxkoo <anton.durcak22@gmail.com>
2022-09-29 15:40:33 +0200

    test

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
-//   transpileDependencies: true
-//   // css: {
-//   //   loaderOptions: {
-//   //     sass: {
-//   //       prependData: `
-//   //         @import '@/assets/s'
-//   //       `
-//   //     }
-//   //   }
-//   // }
-// })
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
-};
-=======
 module.exports = {
   publicPath: "/weather-app/"
 };
->>>>>>> build
+
